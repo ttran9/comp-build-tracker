1382b915f96e02827fa32a8d5bebac4bc00226f2
jstran9 <wptran58@gmail.com>
2019-07-23 16:50:57 -0700

    Adding comments to tests.

--- comp-build-backend/src/test/java/tran/compbuildbackend/services/security/ApplicationUserAuthenticationServiceImplTest.java
+++ comp-build-backend/src/test/java/tran/compbuildbackend/services/security/ApplicationUserAuthenticationServiceImplTest.java
@@ -11,8 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.context.junit4.SpringRunner;
-import tran.compbuildbackend.payload.JWTLoginSuccessResponse;
-import tran.compbuildbackend.payload.LoginRequest;
+import tran.compbuildbackend.payload.email.JWTLoginSuccessResponse;
+import tran.compbuildbackend.payload.email.LoginRequest;
 import tran.compbuildbackend.security.JwtTokenProvider;
 
 import static org.junit.Assert.*;
@@ -37,6 +37,9 @@ public class ApplicationUserAuthenticationServiceImplTest {
         jwtTokenProvider = new JwtTokenProvider();
     }
 
+    /*
+     * This test will check for a successful authentication with no errors expected.
+     */
     @Test
     public void testSuccessfulAuthentication() {
         LoginRequest loginRequest = new LoginRequest(USER_NAME_ONE, USER_PASSWORD);
@@ -48,6 +51,9 @@ public class ApplicationUserAuthenticationServiceImplTest {
         assertTrue(bodyContents.isSuccess());
     }
 
+    /*
+     * This test checks for an attempted authentication that uses the incorrect credentials.
+     */
     @Test(expected = BadCredentialsException.class)
     public void testAuthenticationFailure() {
         LoginRequest loginRequest = new LoginRequest(USER_NAME_ONE, USER_PASSWORD + "1");
