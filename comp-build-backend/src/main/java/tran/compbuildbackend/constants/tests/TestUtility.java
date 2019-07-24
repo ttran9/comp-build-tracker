f0c8e185d75b6ab6a8469ea2fc308408b15c1612
jstran9 <wptran58@gmail.com>
2019-07-24 16:02:29 -0700

    Adding build descriptions constants/fields.

--- comp-build-backend/src/main/java/tran/compbuildbackend/constants/tests/TestUtility.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/constants/tests/TestUtility.java
@@ -6,8 +6,11 @@ public class TestUtility {
 
 
     // computer build test constants.
-    public static final String SAMPLE_COMPUTER_BUILD_NAME = "Budget Comp Build";
+    public static final String SAMPLE_BUDGET_COMPUTER_BUILD_NAME = "Budget Comp Build";
     public static final String SAMPLE_GAMING_COMPUTER_BUILD_NAME = "Gaming Comp Build";
+
+    public static final String SAMPLE_GAMING_COMPUTER_BUILD_DESCRIPTION = "This is my gaming comp build for 2019";
+    public static final String SAMPLE_BUDGET_COMPUTER_BUILD_DESCRIPTION = "This is my budget comp build for 2019!!";
 
     public static final String COMPUTER_BUILD_NAME_REQUIRED_ERROR = "must not be null";
 
