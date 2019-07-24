9a1b24fd64938dc572d81d5effd1f60e9592abcf
jstran9 <wptran58@gmail.com>
2019-07-24 15:59:50 -0700

    Removing method, renaming method, and adding build description.

--- comp-build-backend/src/main/java/tran/compbuildbackend/mapper/ComputerBuildToComputerBuildResponse.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/mapper/ComputerBuildToComputerBuildResponse.java
@@ -3,26 +3,12 @@ package tran.compbuildbackend.mapper;
 import tran.compbuildbackend.payload.computerbuild.ComputerBuildResponse;
 
 public class ComputerBuildToComputerBuildResponse {
-
-    public static ComputerBuildResponse computerBuildToComputerBuildResponseName(String computerName) {
-        ComputerBuildResponse computerBuildResponse = new ComputerBuildResponse();
-        computerBuildResponse.setName(computerName);
-
-        return computerBuildResponse;
-    }
-
-    public static ComputerBuildResponse computerBuildToComputerBuildResponseNameAndBuildIdentifier(String computerName,
-                                                                                                   String buildIdentifier) {
+    public static ComputerBuildResponse buildToBuildResponseWithFields(String computerName, String buildIdentifier,
+                                                                       String buildDescription) {
         ComputerBuildResponse computerBuildResponse = new ComputerBuildResponse();
         computerBuildResponse.setName(computerName);
         computerBuildResponse.setBuildIdentifier(buildIdentifier);
-
-        return computerBuildResponse;
-    }
-
-    public static ComputerBuildResponse computerBuildToComputerBuildResponseMessage(String message) {
-        ComputerBuildResponse computerBuildResponse = new ComputerBuildResponse();
-        computerBuildResponse.setMessage(message);
+        computerBuildResponse.setBuildDescription(buildDescription);
 
         return computerBuildResponse;
     }
