8874a9689eef7b4fcdb0da4ab74ae0c0dec408c0
jstran9 <wptran58@gmail.com>
2019-07-23 17:09:56 -0700

    Adding in extra constants used in tests.

--- comp-build-backend/src/main/java/tran/compbuildbackend/constants/tests/TestUtility.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/constants/tests/TestUtility.java
@@ -3,4 +3,12 @@ package tran.compbuildbackend.constants.tests;
 public class TestUtility {
     public static final String EXPRESSION_CHARACTERS = "$.";
     public static final String BASE_URL = "http://localhost:8080";
+
+
+    // computer build test constants.
+    public static final String SAMPLE_COMPUTER_BUILD_NAME = "Budget Comp Build";
+    public static final String SAMPLE_GAMING_COMPUTER_BUILD_NAME = "Gaming Comp Build";
+
+    public static final String COMPUTER_BUILD_NAME_REQUIRED_ERROR = "must not be null";
+
 }
