ea816ed4baaadeca9c4cccbc6b361e0a2dd6dc25
jstran9 <wptran58@gmail.com>
2019-07-23 17:10:38 -0700

    Renaming constant to be more descriptive.

--- comp-build-backend/src/main/java/tran/compbuildbackend/constants/exception/ExceptionConstants.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/constants/exception/ExceptionConstants.java
@@ -1,7 +1,7 @@
 package tran.compbuildbackend.constants.exception;
 
 public class ExceptionConstants {
-    public static final int EXCEPTION_USER_NAME_DOES_NOT_EXIST = 1;
+    public static final int EXCEPTION_EMAIL_NAME_DOES_NOT_EXIST = 1;
     public static final int EXCEPTION_REQUEST_PASSWORD_CHANGE_FAILED = 2;
 
     // exception messages.
