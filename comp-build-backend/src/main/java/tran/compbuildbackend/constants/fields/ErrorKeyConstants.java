18385000d43fe95efae2b764a78aaa6bc4d1de63
jstran9 <wptran58@gmail.com>
2019-07-23 17:11:36 -0700

    Creating extra constants used in tests.

--- comp-build-backend/src/main/java/tran/compbuildbackend/constants/fields/ErrorKeyConstants.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/constants/fields/ErrorKeyConstants.java
@@ -3,4 +3,6 @@ package tran.compbuildbackend.constants.fields;
 public class ErrorKeyConstants {
     public static final String LENGTH_KEY = "Length";
     public static final String MATCH_KEY = "Match";
+    public static final String INVALID_KEY = "Invalid";
+    public static final String MESSAGE_KEY = "message";
 }
--- comp-build-backend/src/main/java/tran/compbuildbackend/constants/fields/FieldConstants.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/constants/fields/FieldConstants.java
@@ -4,6 +4,7 @@ public class FieldConstants {
     public static final String PASSWORD_FIELD = "password";
     public static final String CONFIRM_PASSWORD_FIELD = "confirmPassword";
     public static final String USER_NAME_FIELD = "username";
+    public static final String EMAIL_FIELD = "email";
     public static final String FULL_NAME_FIELD = "fullName";
     public static final String ID_FIELD = "id";
 }
