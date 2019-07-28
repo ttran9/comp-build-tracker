05a4bdcf34b87422d182e5aead74e04a18e61b76
jstran9 <wptran58@gmail.com>
2019-07-27 17:46:30 -0700

    Adding actions for the computer build.

--- comp-build-frontend/src/actions/types.js
+++ comp-build-frontend/src/actions/types.js
@@ -1,7 +1,13 @@
+// Type(s) for errors.
 export const GET_ERRORS = "GET_ERRORS";
 
-// Types for users.
+// Type(s) for users.
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 
-// Type for events.
+// Type(s) for events.
 export const REQUEST_SUCCESS = "REQUEST_SUCCESS";
+
+// Type(s) for computerBuild.
+export const GET_COMPUTERBUILDS = "GET_COMPUTERBUILDS";
+export const GET_COMPUTERBUILD = "GET_COMPUTERBUILD";
+export const DELETE_COMPUTERBUILD = "DELETE_COMPUTERBUILD";
