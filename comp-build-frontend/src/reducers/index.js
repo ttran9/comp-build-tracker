266043e8c58c735e8d8b45b05a9929b69834a899
jstran9 <wptran58@gmail.com>
2019-07-27 17:48:41 -0700

    Adding the computerbuild reducer.

--- comp-build-frontend/src/reducers/index.js
+++ comp-build-frontend/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import securityReducer from "./securityReducer";
+import computerBuildReducer from "./computerBuildReducer";
 
 export default combineReducers({
   // returns all the reducers
 
   errors: errorReducer,
-  security: securityReducer
+  security: securityReducer,
+  computerBuild: computerBuildReducer
 });
