23363307f7c2c0ecbfdbf17a7482d2fde9c84152
jstran9 <wptran58@gmail.com>
2019-07-27 18:14:38 -0700

    Adding annotation for explicit bean creation.

--- comp-build-backend/src/main/java/tran/compbuildbackend/repositories/users/ApplicationUserRepository.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/repositories/users/ApplicationUserRepository.java
@@ -1,8 +1,10 @@
 package tran.compbuildbackend.repositories.users;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import tran.compbuildbackend.domain.user.ApplicationUser;
 
+@Repository
 public interface ApplicationUserRepository extends CrudRepository<ApplicationUser, Long> {
     ApplicationUser findByUsername(String username);
     ApplicationUser findByEmail(String email);
