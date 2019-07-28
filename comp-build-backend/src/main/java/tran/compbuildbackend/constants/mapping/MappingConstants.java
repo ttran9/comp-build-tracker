a6873ab46974b4d9889624de340c6436496e7e1e
jstran9 <wptran58@gmail.com>
2019-07-27 18:15:40 -0700

    Modifying the regex to allow for mapping and being able to handle a request from the browser (i.e. user typing in the URL).

--- comp-build-backend/src/main/java/tran/compbuildbackend/constants/mapping/MappingConstants.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/constants/mapping/MappingConstants.java
@@ -3,7 +3,7 @@ package tran.compbuildbackend.constants.mapping;
 public class MappingConstants {
 
     public static final String URLS_REGEX = "^\\/[a-zA-Z]+\\/{0,1}$";
-    public static final String PB_UPT_REGEX = "\\/[a-zA-Z]+\\/[a-zA-Z0-9]{1,5}";
+    public static final String PB_UPT_REGEX = "\\/[a-zA-Z]+\\/[a-z0-9]{1,8}";
 
     // ApplicationUserController
     public static final String CONFIRM_REGISTRATION_URL = "/confirmRegistration/";
