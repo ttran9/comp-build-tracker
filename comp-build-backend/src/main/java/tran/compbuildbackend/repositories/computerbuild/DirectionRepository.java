082c3ac081ff79337e61c630b43d130bb0fe3616
jstran9 <wptran58@gmail.com>
2019-07-27 18:07:52 -0700

    Creating repositories to perform CRUD operations.

---
+++ comp-build-backend/src/main/java/tran/compbuildbackend/repositories/computerbuild/DirectionRepository.java
@@ -0,0 +1,9 @@
+package tran.compbuildbackend.repositories.computerbuild;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import tran.compbuildbackend.domain.computerbuild.Direction;
+
+@Repository
+public interface DirectionRepository extends CrudRepository<Direction, Long> {
+}
---
+++ comp-build-backend/src/main/java/tran/compbuildbackend/repositories/computerbuild/PurposeRepository.java
@@ -0,0 +1,9 @@
+package tran.compbuildbackend.repositories.computerbuild;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import tran.compbuildbackend.domain.computerbuild.Purpose;
+
+@Repository
+public interface PurposeRepository extends CrudRepository<Purpose, Long> {
+}
