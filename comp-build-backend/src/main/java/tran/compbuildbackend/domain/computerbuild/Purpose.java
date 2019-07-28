362465bd6ac26a710da3f17c22f6b99e8ad4bad5
jstran9 <wptran58@gmail.com>
2019-07-27 18:24:18 -0700

    Refactoring purpose to inherit from an abstract note (making it have the same basic structure/functions as overclocking note and build note).

--- comp-build-backend/src/main/java/tran/compbuildbackend/domain/computerbuild/Purpose.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/domain/computerbuild/Purpose.java
@@ -1,61 +1,18 @@
 package tran.compbuildbackend.domain.computerbuild;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "purpose")
-public class Purpose {
+public class Purpose extends AbstractNote {
 
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "computer_build_id", updatable = false, nullable = false)
-    private ComputerBuild computerBuild;
-
-    @NotNull
-    @Lob
-    private String description;
-
-    @Min(1)
-    @Max(3)
-    private int priority;
-
-    public Purpose() { }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public ComputerBuild getComputerBuild() {
-        return computerBuild;
-    }
-
-    public void setComputerBuild(ComputerBuild computerBuild) {
-        this.computerBuild = computerBuild;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
+    public Purpose() {
+        super();
     }
 }
