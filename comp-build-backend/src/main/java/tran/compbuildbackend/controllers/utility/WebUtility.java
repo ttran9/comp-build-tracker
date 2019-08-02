e91df694c4f86d4db9cf0f6d447d156ef54c0c8f
jstran9 <wptran58@gmail.com>
2019-08-02 00:30:06 -0700

    Refactoring login method to be able to be accessed from the tests.

--- comp-build-backend/src/main/java/tran/compbuildbackend/controllers/utility/WebUtility.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/controllers/utility/WebUtility.java
@@ -1,12 +1,25 @@
 package tran.compbuildbackend.controllers.utility;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
+import tran.compbuildbackend.domain.utility.ApplicationUserUtility;
+import tran.compbuildbackend.payload.email.JWTLoginSuccessResponse;
 import tran.compbuildbackend.payload.email.LoginRequest;
 import tran.compbuildbackend.security.JwtTokenProvider;
 import tran.compbuildbackend.services.security.ApplicationUserAuthenticationService;
+
+import java.net.URI;
+import java.util.LinkedHashMap;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static tran.compbuildbackend.constants.mapping.MappingConstants.LOGIN_URL;
+import static tran.compbuildbackend.constants.mapping.MappingConstants.USERS_API;
+import static tran.compbuildbackend.constants.tests.TestUtility.BASE_URL;
 
 public class WebUtility {
 
@@ -34,6 +47,32 @@ public class WebUtility {
     }
 
     /**
+     * helper method to post to the login URL as well as return the token from the successful login.
+     * @param userName The username of the user.
+     * @param password The password of the user logging in.
+     * @param restTemplate An object to assist with making the POST request to the login endpoint.
+     * @return The JWT token upon successful login, an exception is expected to be thrown if there is an error.
+     * @throws Exception If there is an issue with posting to the login URL successfully.
+     */
+    public static String loginHelper(String userName, String password, TestRestTemplate restTemplate) throws Exception {
+        String loginContent = ApplicationUserUtility.getLoginRequestAsJson(userName, password);
+        String loginURL = BASE_URL + USERS_API + LOGIN_URL;
+        URI uri = new URI(loginURL);
+
+        ResponseEntity<Object> result = restTemplate.exchange(uri, HttpMethod.POST, WebUtility.getEntity(loginContent), Object.class);
+        Object loginSuccessResponse = result.getBody();
+        LinkedHashMap response = (LinkedHashMap) loginSuccessResponse;
+        Gson gson = new Gson();
+        String json = gson.toJson(response,LinkedHashMap.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JWTLoginSuccessResponse loginResponse = objectMapper.readValue(json, JWTLoginSuccessResponse.class);
+
+        assertNotNull(loginResponse.getToken());
+        assertTrue(loginResponse.getSuccess());
+        return loginResponse.getToken().substring(7);
+    }
+
+    /**
      * A helper method used in our tests in order to log the user in. This will add the authenticated user to Spring's
      * security context.
      * @param applicationUserAuthenticationService The authentication service to assist with authenticating credentials.
@@ -44,5 +83,9 @@ public class WebUtility {
     public static void logUserIn(ApplicationUserAuthenticationService applicationUserAuthenticationService, AuthenticationManager authenticationManager,
                             JwtTokenProvider jwtTokenProvider, LoginRequest loginRequest) {
         applicationUserAuthenticationService.applicationUserAuthentication(loginRequest, authenticationManager, jwtTokenProvider);
+    }
+
+    public static void logUserOut() {
+        SecurityContextHolder.getContext().setAuthentication(null);
     }
 }
