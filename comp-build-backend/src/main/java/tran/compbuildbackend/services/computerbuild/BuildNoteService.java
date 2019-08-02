87d96ba2192e1446fb8d3fb1ce86b8f91a8dbea6
jstran9 <wptran58@gmail.com>
2019-08-02 00:40:52 -0700

    Changing services to implement a base computer build detail service.

--- comp-build-backend/src/main/java/tran/compbuildbackend/services/computerbuild/BuildNoteService.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/services/computerbuild/BuildNoteService.java
@@ -1,14 +1,6 @@
 package tran.compbuildbackend.services.computerbuild;
 
 import tran.compbuildbackend.domain.computerbuild.BuildNote;
-import tran.compbuildbackend.domain.computerbuild.ComputerBuild;
 
-public interface BuildNoteService {
-    /**
-     * Adds a build note object to the database.
-     * @param computerBuild The computer build object to be associated with the AbstractNote.
-     * @param buildNote The build note to be persisted.
-     * @return Returns the persisted build note object linked/associated with the ComputerBuild.
-     */
-    BuildNote create(ComputerBuild computerBuild, BuildNote buildNote);
+public interface BuildNoteService extends ComputerBuildDetailService<BuildNote> {
 }
--- comp-build-backend/src/main/java/tran/compbuildbackend/services/computerbuild/ComputerPartService.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/services/computerbuild/ComputerPartService.java
@@ -1,16 +1,7 @@
 package tran.compbuildbackend.services.computerbuild;
 
-import tran.compbuildbackend.domain.computerbuild.ComputerBuild;
 import tran.compbuildbackend.domain.computerbuild.ComputerPart;
 
-public interface ComputerPartService {
-
-    /**
-     * Adds a computer part object to the database.
-     * @param computerBuild The computer build object to be associated with the ComputerPart.
-     * @param computerPart The computer part to be saved.
-     * @return Returns the persisted computer part object linked/associated with the ComputerBuild.
-     */
-    ComputerPart createComputerPart(ComputerBuild computerBuild, ComputerPart computerPart);
+public interface ComputerPartService extends ComputerBuildDetailService<ComputerPart> {
 
 }
