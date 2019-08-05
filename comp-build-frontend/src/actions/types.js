7f0e4a4c9ab52534d4d2a5fd0a4b6cd4be27fc01
jstran9 <wptran58@gmail.com>
2019-08-05 02:27:41 -0700

    Creating action types for state changes.

--- comp-build-frontend/src/actions/types.js
+++ comp-build-frontend/src/actions/types.js
@@ -11,3 +11,26 @@ export const REQUEST_SUCCESS = "REQUEST_SUCCESS";
 export const GET_COMPUTERBUILDS = "GET_COMPUTERBUILDS";
 export const GET_COMPUTERBUILD = "GET_COMPUTERBUILD";
 export const DELETE_COMPUTERBUILD = "DELETE_COMPUTERBUILD";
+
+// Type(s) for computer build details
+export const GET_COMPUTER_BUILD_DETAILS = "GET_COMPUTER_BUILD_DETAILS";
+
+// Type(s) for computer part.
+export const DELETE_COMPUTERPART = "DELETE_COMPUTERPART";
+export const GET_COMPUTERPART = "GET_COMPUTERPART";
+
+// Type(s) for purpose.
+export const DELETE_PURPOSE = "DELETE_PURPOSE";
+export const GET_PURPOSE = "GET_PURPOSE";
+
+// Type(s) for build note.
+export const DELETE_BUILD_NOTE = "DELETE_BUILD_NOTE";
+export const GET_BUILD_NOTE = "GET_BUILD_NOTE";
+
+// Type(s) for direction.
+export const DELETE_DIRECTION = "DELETE_DIRECTION";
+export const GET_DIRECTION = "GET_DIRECTION";
+
+// Type(s) for overclocking note.
+export const DELETE_OVERCLOCKING_NOTE = "DELETE_OVERCLOCKING_NOTE";
+export const GET_OVERCLOCKING_NOTE = "GET_OVERCLOCKING_NOTE";
