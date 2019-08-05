7109d2d575e09508bdc87d7797b3fd9f3bcf4ded
jstran9 <wptran58@gmail.com>
2019-08-05 02:24:47 -0700

    Adding in reducer for the computer build details (purpose, overclocking notes, other notes, directions, and computer parts).

--- comp-build-frontend/src/reducers/index.js
+++ comp-build-frontend/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import securityReducer from "./securityReducer";
 import computerBuildReducer from "./computerBuildReducer";
+import computerBuildDetailReducer from "./computerBuildDetailReducer";
 
 export default combineReducers({
   // returns all the reducers
 
   errors: errorReducer,
   security: securityReducer,
-  computerBuild: computerBuildReducer
+  computerBuild: computerBuildReducer,
+  computerBuildDetails: computerBuildDetailReducer
 });
