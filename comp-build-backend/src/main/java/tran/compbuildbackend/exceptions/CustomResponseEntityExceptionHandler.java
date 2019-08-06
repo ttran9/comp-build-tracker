a6006cd889a3f8d52a1e45b9a81d25d7d27dc9f5
jstran9 <wptran58@gmail.com>
2019-08-06 00:42:21 -0700

    Removing redundant custom exceptions and adding additional logic to account for different types of errors/exceptions.

--- comp-build-backend/src/main/java/tran/compbuildbackend/exceptions/CustomResponseEntityExceptionHandler.java
+++ comp-build-backend/src/main/java/tran/compbuildbackend/exceptions/CustomResponseEntityExceptionHandler.java
@@ -8,24 +8,19 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import tran.compbuildbackend.exceptions.computerbuild.*;
-import tran.compbuildbackend.exceptions.request.*;
-import tran.compbuildbackend.exceptions.security.*;
+import tran.compbuildbackend.exceptions.request.GenericRequestException;
+import tran.compbuildbackend.exceptions.request.GenericRequestExceptionResponse;
+import tran.compbuildbackend.exceptions.request.MultipleFieldsException;
+import tran.compbuildbackend.exceptions.request.MultipleFieldsExceptionResponse;
+import tran.compbuildbackend.exceptions.security.InvalidLoginExceptionResponse;
 
 import java.util.Map;
 
-import static tran.compbuildbackend.constants.fields.FieldConstants.EMAIL_FIELD;
-import static tran.compbuildbackend.constants.fields.FieldConstants.USER_NAME_FIELD;
+import static tran.compbuildbackend.constants.fields.FieldConstants.*;
 
 @ControllerAdvice
 @RestController
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleDuplicateUsername(UsernameDuplicateException ex) {
-        UsernameDuplicateExceptionResponse duplicateUserNameException = new UsernameDuplicateExceptionResponse(ex.getMessage());
-        return new ResponseEntity<>(duplicateUserNameException, HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler
     public final ResponseEntity<Object> handleInvalidLogin(BadCredentialsException ex) {
         InvalidLoginExceptionResponse invalidLoginExceptionResponse = new InvalidLoginExceptionResponse();
@@ -39,79 +34,58 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
     }
 
     @ExceptionHandler
-    public final ResponseEntity<Object> handleUsernameCreationException(UsernameCreationErrorException ex) {
-        UsernameCreationErrorExceptionResponse usernameCreationErrorExceptionResponse = new UsernameCreationErrorExceptionResponse(ex.getMessage());
-        return new ResponseEntity<>(usernameCreationErrorExceptionResponse, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleEmailVerificationTokenException(EmailVerificationTokenException ex) {
-        EmailVerificationTokenExceptionResponse response = new EmailVerificationTokenExceptionResponse(ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleRequestPasswordChangeException(RequestChangePasswordException ex) {
-        RequestChangePasswordExceptionResponse response = new RequestChangePasswordExceptionResponse(ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleChangePasswordTokenException(ChangePasswordTokenException ex) {
-        ChangePasswordTokenExceptionResponse response = new ChangePasswordTokenExceptionResponse();
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
     public final ResponseEntity<Object> handleGenericRequestException(GenericRequestException ex) {
-        GenericRequestExceptionResponse response = new GenericRequestExceptionResponse(ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
+        GenericRequestExceptionResponse response = new GenericRequestExceptionResponse();
 
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleEmailRequestException(EmailRequestException ex) {
-        EmailRequestExceptionResponse response = new EmailRequestExceptionResponse(ex.getMessage());
+        if(ex.getMessage() != null) {
+            response.setMessage(ex.getMessage());
+        }
+        if(ex.getUsername() != null) {
+            response.setUsername(ex.getUsername());
+        }
+        if(ex.getToken() != null) {
+            response.setToken(ex.getToken());
+        }
+        if(ex.getPassword() != null) {
+            response.setPassword(ex.getPassword());
+        }
+
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public final ResponseEntity<Object> handleMultipleFieldsException(MultipleFieldsException ex) {
         Map<String, String> errors = ex.getMapError();
-
         MultipleFieldsExceptionResponse response = new MultipleFieldsExceptionResponse();
 
         if(errors.get(USER_NAME_FIELD) != null) {
             response.setUsername(errors.get(USER_NAME_FIELD));
         }
-
         if(errors.get(EMAIL_FIELD) != null) {
             response.setEmail(errors.get(EMAIL_FIELD));
         }
+        if(errors.get(FULL_NAME_FIELD) != null) {
+            response.setFullName(errors.get(FULL_NAME_FIELD));
+        }
+        if(errors.get(PASSWORD_FIELD) != null) {
+            response.setPassword(errors.get(PASSWORD_FIELD));
+        }
+        if(errors.get(CONFIRM_PASSWORD_FIELD) != null) {
+            response.setConfirmPassword(errors.get(CONFIRM_PASSWORD_FIELD));
+        }
+        if(errors.get(PLACE_PURCHASED_AT_FIELD) != null) {
+            response.setPlacePurchasedAt(errors.get(PLACE_PURCHASED_AT_FIELD));
+        }
+        if(errors.get(NAME_FIELD) != null) {
+            response.setName(errors.get(NAME_FIELD));
+        }
+        if(errors.get(PRICE_FIELD) != null) {
+            response.setPrice(errors.get(PRICE_FIELD));
+        }
+        if(errors.get(DESCRIPTION_FIELD) != null) {
+            response.setDescription(errors.get(DESCRIPTION_FIELD));
+        }
 
-        /*
-         * TODO: add in other fields that could be incorrect on form submission from a variety of pages.
-         * some pages: registration (fullname, username, email, password, confirm password), login (username, password),
-         * create computer build (computer build name, computer build description), etc.
-         */
-
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleNoteException(NoteException ex) {
-        NoteExceptionResponse response = new NoteExceptionResponse(ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleComputerDetailIdentifierException(ComputerDetailIdentifierException ex) {
-        ComputerDetailIdentifierExceptionResponse response = new ComputerDetailIdentifierExceptionResponse(ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public final ResponseEntity<Object> handleComputerBuildOwnerException(ComputerBuildOwnerException ex) {
-        ComputerBuildOwnerExceptionResponse response = new ComputerBuildOwnerExceptionResponse(ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
