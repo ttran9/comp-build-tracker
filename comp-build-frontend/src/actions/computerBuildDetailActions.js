e37fbba206d72f0f099f2329b5ad03de511e9e1f
jstran9 <wptran58@gmail.com>
2019-08-06 17:19:40 -0700

    Compressing repetitive functions.

--- comp-build-frontend/src/actions/computerBuildDetailActions.js
+++ comp-build-frontend/src/actions/computerBuildDetailActions.js
@@ -14,6 +14,7 @@ import {
   DELETE_PURPOSE,
   GET_PURPOSE
 } from "./types";
+import { clearErrors } from "../errorUtils/clearErrors";
 
 // computer build detail actions start
 /*
@@ -42,24 +43,18 @@ export const deleteComputerBuildByBuildIdentifier = buildIdentifier => async dis
 };
 // computer build detail actions end
 
-// computer part actions start
-export const createComputerPart = (
-  computerPart,
+// CRUD operations for computer build details (computer parts, purpose, build note, direction, and overclocking note) start.
+export const createObject = (
+  object,
   history,
+  apiURL,
   computerBuildIdentifier
 ) => async dispatch => {
   try {
-    await axios.post(
-      `${Constants.COMPUTER_PART_API}${computerBuildIdentifier}`,
-      computerPart
-    );
-    // go back to the computer build detail page where the created computer part will be displayed.
+    await axios.post(`${apiURL}${computerBuildIdentifier}`, object);
+    // go back to the computer build detail page where the created object will be displayed.
     history.push(`${Constants.COMPUTER_BUILD_URL}${computerBuildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-    // clearErrors(dispatch);
+    clearErrors(dispatch);
   } catch (error) {
     dispatch({
       type: GET_ERRORS,
@@ -68,39 +63,37 @@ export const createComputerPart = (
   }
 };
 
-export const deleteComputerPartById = (
+export const deleteObjectById = (
+  apiURL,
   buildIdentifier,
-  computerPartIdentifier
+  uniqueIdentifier,
+  dispatchType
 ) => async dispatch => {
   if (
     window.confirm(
-      `You are deleting this computer part, this action cannot be undone.`
+      `You are deleting the selected object, this action cannot be undone.`
     )
   ) {
-    await axios.delete(
-      `${
-        Constants.COMPUTER_PART_API
-      }${buildIdentifier}/${computerPartIdentifier}`
-    );
+    await axios.delete(`${apiURL}${buildIdentifier}/${uniqueIdentifier}`);
     dispatch({
-      type: DELETE_COMPUTERPART,
-      payload: computerPartIdentifier
+      type: dispatchType,
+      payload: uniqueIdentifier
     });
   }
 };
 
-export const getComputerPartById = (
+export const getObjectById = (
+  apiURL,
   buildIdentifier,
-  computerPartIdentifier
+  uniqueIdentifier,
+  dispatchType
 ) => async dispatch => {
   try {
     const response = await axios.get(
-      `${
-        Constants.COMPUTER_PART_API
-      }${buildIdentifier}/${computerPartIdentifier}`
+      `${apiURL}${buildIdentifier}/${uniqueIdentifier}`
     );
     dispatch({
-      type: GET_COMPUTERPART,
+      type: dispatchType,
       payload: response.data
     });
   } catch (error) {
@@ -111,114 +104,20 @@ export const getComputerPartById = (
   }
 };
 
-export const updateComputerPart = (
-  computerPart,
+export const updateObject = (
+  object,
   buildIdentifier,
-  computerPartIdentifier,
-  history
-) => async dispatch => {
-  try {
-    await axios.patch(
-      `${
-        Constants.COMPUTER_PART_API
-      }${buildIdentifier}/${computerPartIdentifier}`,
-      computerPart
-    );
-    history.push(`${Constants.COMPUTER_BUILD_URL}${buildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-// computer part actions end
-
-// purpose actions start
-export const createPurpose = (
-  purpose,
+  uniqueIdentifier,
   history,
-  computerBuildIdentifier
-) => async dispatch => {
-  try {
-    await axios.post(
-      `${Constants.PURPOSE_API}${computerBuildIdentifier}`,
-      purpose
-    );
-    // go back to the computer build detail page where the new purpose will be displayed.
-    history.push(`${Constants.COMPUTER_BUILD_URL}${computerBuildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-    // clearErrors(dispatch);
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-
-export const deletePurposeById = (
-  buildIdentifier,
-  purposeIdentifier
-) => async dispatch => {
-  if (
-    window.confirm(
-      `You are deleting this purpose, this action cannot be undone.`
-    )
-  ) {
-    await axios.delete(
-      `${Constants.PURPOSE_API}${buildIdentifier}/${purposeIdentifier}`
-    );
-    dispatch({
-      type: DELETE_PURPOSE,
-      payload: purposeIdentifier
-    });
-  }
-};
-
-export const getPurposeById = (
-  buildIdentifier,
-  purposeIdentifier
-) => async dispatch => {
-  try {
-    const response = await axios.get(
-      `${Constants.PURPOSE_API}${buildIdentifier}/${purposeIdentifier}`
-    );
-    dispatch({
-      type: GET_PURPOSE,
-      payload: response.data
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-
-export const updatePurpose = (
-  purpose,
-  buildIdentifier,
-  purposeIdentifier,
-  history
+  apiURL
 ) => async dispatch => {
   try {
     await axios.patch(
-      `${Constants.PURPOSE_API}${buildIdentifier}/${purposeIdentifier}`,
-      purpose
+      `${apiURL}${buildIdentifier}/${uniqueIdentifier}`,
+      object
     );
     history.push(`${Constants.COMPUTER_BUILD_URL}${buildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
+    clearErrors(dispatch);
   } catch (error) {
     dispatch({
       type: GET_ERRORS,
@@ -226,280 +125,4 @@ export const updatePurpose = (
     });
   }
 };
-// purpose actions end
-
-// build note actions start
-export const createBuildNote = (
-  buildNote,
-  history,
-  computerBuildIdentifier
-) => async dispatch => {
-  try {
-    await axios.post(
-      `${Constants.BUILD_NOTE_API}${computerBuildIdentifier}`,
-      buildNote
-    );
-    // go back to the computer build detail page where the new build note will be displayed.
-    history.push(`${Constants.COMPUTER_BUILD_URL}${computerBuildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-    // clearErrors(dispatch);
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-
-export const deleteBuildNoteById = (
-  buildIdentifier,
-  buildNoteIdentifier
-) => async dispatch => {
-  if (
-    window.confirm(
-      `You are deleting this build note, this action cannot be undone.`
-    )
-  ) {
-    await axios.delete(
-      `${Constants.BUILD_NOTE_API}${buildIdentifier}/${buildNoteIdentifier}`
-    );
-    dispatch({
-      type: DELETE_BUILD_NOTE,
-      payload: buildNoteIdentifier
-    });
-  }
-};
-
-export const getBuildNoteById = (
-  buildIdentifier,
-  buildNoteIdentifier
-) => async dispatch => {
-  try {
-    const response = await axios.get(
-      `${Constants.BUILD_NOTE_API}${buildIdentifier}/${buildNoteIdentifier}`
-    );
-    dispatch({
-      type: GET_BUILD_NOTE,
-      payload: response.data
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-
-export const updateBuildNote = (
-  buildNote,
-  buildIdentifier,
-  buildNoteIdentifier,
-  history
-) => async dispatch => {
-  try {
-    await axios.patch(
-      `${Constants.BUILD_NOTE_API}${buildIdentifier}/${buildNoteIdentifier}`,
-      buildNote
-    );
-    history.push(`${Constants.COMPUTER_BUILD_URL}${buildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-// build note actions end
-
-// direction actions start
-export const createDirection = (
-  direction,
-  history,
-  computerBuildIdentifier
-) => async dispatch => {
-  try {
-    await axios.post(
-      `${Constants.DIRECTION_API}${computerBuildIdentifier}`,
-      direction
-    );
-    // go back to the computer build detail page where the new direction will be displayed.
-    history.push(`${Constants.COMPUTER_BUILD_URL}${computerBuildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-    // clearErrors(dispatch);
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-
-export const deleteDirectionById = (
-  buildIdentifier,
-  directionIdentifier
-) => async dispatch => {
-  if (
-    window.confirm(
-      `You are deleting this direction, this action cannot be undone.`
-    )
-  ) {
-    await axios.delete(
-      `${Constants.DIRECTION_API}${buildIdentifier}/${directionIdentifier}`
-    );
-    dispatch({
-      type: DELETE_DIRECTION,
-      payload: directionIdentifier
-    });
-  }
-};
-
-export const getDirectionById = (
-  buildIdentifier,
-  directionIdentifier
-) => async dispatch => {
-  try {
-    const response = await axios.get(
-      `${Constants.DIRECTION_API}${buildIdentifier}/${directionIdentifier}`
-    );
-    dispatch({
-      type: GET_DIRECTION,
-      payload: response.data
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-
-export const updateDirection = (
-  direction,
-  buildIdentifier,
-  directionIdentifier,
-  history
-) => async dispatch => {
-  try {
-    await axios.patch(
-      `${Constants.DIRECTION_API}${buildIdentifier}/${directionIdentifier}`,
-      direction
-    );
-    history.push(`${Constants.COMPUTER_BUILD_URL}${buildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-// direction actions end
-
-// overclocking note actions start
-export const createOverclockingNote = (
-  overclockingNote,
-  history,
-  computerBuildIdentifier
-) => async dispatch => {
-  try {
-    await axios.post(
-      `${Constants.OVERCLOCKING_NOTE_API}${computerBuildIdentifier}`,
-      overclockingNote
-    );
-    // go back to the computer build detail page where the new overclocking note will be displayed.
-    history.push(`${Constants.COMPUTER_BUILD_URL}${computerBuildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-    // clearErrors(dispatch);
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-
-export const deleteOverclockingNoteById = (
-  buildIdentifier,
-  overclockingNoteIdentifier
-) => async dispatch => {
-  if (
-    window.confirm(
-      `You are deleting this overclocking note, this action cannot be undone.`
-    )
-  ) {
-    await axios.delete(
-      `${
-        Constants.OVERCLOCKING_NOTE_API
-      }${buildIdentifier}/${overclockingNoteIdentifier}`
-    );
-    dispatch({
-      type: DELETE_OVERCLOCKING_NOTE,
-      payload: overclockingNoteIdentifier
-    });
-  }
-};
-
-export const getOverclockingNoteById = (
-  buildIdentifier,
-  overclockingNoteIdentifier
-) => async dispatch => {
-  try {
-    const response = await axios.get(
-      `${
-        Constants.OVERCLOCKING_NOTE_API
-      }${buildIdentifier}/${overclockingNoteIdentifier}`
-    );
-    dispatch({
-      type: GET_OVERCLOCKING_NOTE,
-      payload: response.data
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-
-export const updateOverclockingNote = (
-  overclockingNote,
-  buildIdentifier,
-  overclockingNoteIdentifier,
-  history
-) => async dispatch => {
-  try {
-    await axios.patch(
-      `${
-        Constants.OVERCLOCKING_NOTE_API
-      }${buildIdentifier}/${overclockingNoteIdentifier}`,
-      overclockingNote
-    );
-    history.push(`${Constants.COMPUTER_BUILD_URL}${buildIdentifier}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: error.response.data
-    });
-  }
-};
-// overclocking note actions end
+// CRUD operations for computer build details (computer parts, purpose, build note, direction, and overclocking note) end.
