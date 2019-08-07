6a5f1c89674e33c2ea2ecc6d0665cc590e970083
jstran9 <wptran58@gmail.com>
2019-08-06 17:17:33 -0700

    Helper function to clear the errors out of the application state.

--- comp-build-frontend/src/errorUtils/clearErrors.js
+++ comp-build-frontend/src/errorUtils/clearErrors.js
@@ -1,5 +1,7 @@
+import { GET_ERRORS } from "../actions/types";
+
 export const clearErrors = dispatch => {
-  // helper function to clear errors after successful form submission.
+  // helper function to clear errors from the application's state after successful form submission.
   dispatch({
     type: GET_ERRORS,
     payload: {}
